5dd01a8f48853ef28f40e22bb6bab258fd19ce9b
kimo1ucl <kimo1@ucl.dk>
2022-09-28 09:55:44 +0200

    Refactored to new method

---
+++ GameRules.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RollTheDiceLearn
+{
+    internal class GameRules
+    {
+        public static bool GameMaster(int dice_1_1,
+            int dice_1_2,
+            int dice_2_1,
+            int dice_2_2)
+        {
+            bool proceed = true;
+            bool player_equal;
+            bool opponent_equal;
+            int countEquals = 0;
+            player_equal = dice_1_1 == dice_1_2;
+            opponent_equal = dice_2_1 == dice_2_2;
+            if (player_equal)
+            {
+                countEquals++;
+            }
+            if (opponent_equal)
+            {
+                countEquals++;
+            }
+
+            if (countEquals == 1)
+            {
+                if (player_equal)
+                {
+                    Console.WriteLine("You win!");
+                }
+                if (opponent_equal)
+                {
+                    Console.WriteLine("I win!");
+                }
+                proceed = false;
+            }
+
+            if (countEquals == 2)
+            {
+                if ((dice_1_1 + dice_1_2) > (dice_2_1 + dice_2_2))
+                {
+                    Console.WriteLine("You win!");
+                    proceed = false;
+                }
+                if ((dice_1_1 + dice_1_2) < (dice_2_1 + dice_2_2))
+                {
+                    Console.WriteLine("I win!");
+                    proceed = false;
+                }
+                if ((dice_1_1 + dice_1_2) == (dice_2_1 + dice_2_2))
+                {
+                    Console.WriteLine("Draw! Continuing");
+                }
+            }
+            return proceed;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -66,63 +66,12 @@ namespace RollTheDiceLearn
 
 
                 // Kald til metode
-                proceed = GameMaster(dice_1_1, dice_1_2, dice_2_1, dice_2_2);
+                proceed = GameRules.GameMaster(dice_1_1, dice_1_2, dice_2_1, dice_2_2);
 
                 Console.ReadLine();
             }
         }
 
-        static bool GameMaster(int dice_1_1,
-            int dice_1_2,
-            int dice_2_1, 
-            int dice_2_2) 
-        {
-            bool proceed = true;
-            bool player_equal;
-            bool opponent_equal;
-            int countEquals = 0;
-            player_equal = dice_1_1 == dice_1_2;
-            opponent_equal = dice_2_1 == dice_2_2;
-            if (player_equal)
-            {
-                countEquals++;
-            }
-            if (opponent_equal)
-            {
-                countEquals++;
-            }
-
-            if (countEquals == 1)
-            {
-                if (player_equal)
-                {
-                    Console.WriteLine("You win!");
-                }
-                if (opponent_equal)
-                {
-                    Console.WriteLine("I win!");
-                }
-                proceed = false;
-            }
-
-            if (countEquals == 2)
-            {
-                if ((dice_1_1 + dice_1_2) > (dice_2_1 + dice_2_2))
-                {
-                    Console.WriteLine("You win!");
-                    proceed = false;
-                }
-                if ((dice_1_1 + dice_1_2) < (dice_2_1 + dice_2_2))
-                {
-                    Console.WriteLine("I win!");
-                    proceed = false;
-                }
-                if ((dice_1_1 + dice_1_2) == (dice_2_1 + dice_2_2))
-                {
-                    Console.WriteLine("Draw! Continuing");
-                }
-            }
-            return proceed;
-        }
+        
     }
 }
